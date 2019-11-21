fd13996554ff345b7413537b10bbec9722790c6d
Ernando Kasaluhe <ernandokasaluhe@ti0171a401-0390.bb.online.no>
2019-11-21 11:37:18 +0700

    First commit

---
+++ TechnicalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechnicalTest
+//
+//  Created by Ernando Kasaluhe on 21/11/19.
+//  Copyright © 2019 Test Coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
